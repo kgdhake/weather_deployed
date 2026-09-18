e5d889206ec5d7ae2a56ebf8080bf46c644fb8bf
gsgit123 <gauravexp123@gmail.com>
2026-09-19 00:30:42 +0530

    fix: update map tile style for Vercel deployment

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -31,7 +31,32 @@ function App() {
   useEffect(() => {
     const map = new MapLibreMap({
       container: mapContainerRef.current,
-      style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+      style: {
+        version: 8,
+        sources: {
+          "carto-dark": {
+            type: "raster",
+            tiles: [
+              "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+              "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+              "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png",
+              "https://d.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png"
+            ],
+            tileSize: 256,
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          },
+        },
+        layers: [
+          {
+            id: "carto-dark-layer",
+            type: "raster",
+            source: "carto-dark",
+            minzoom: 0,
+            maxzoom: 19,
+          },
+        ],
+      },
       center: [78.9629, 20.5937], // Center on India
       zoom: 4,
       attributionControl: true,
